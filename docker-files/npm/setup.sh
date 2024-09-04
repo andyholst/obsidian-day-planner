8483aa1866d75b5c6a8901a484fa486f55239260
Andy Holst <andy.holst@protonmail.com>
2024-09-05 01:28:15 +0200

    ...

--- docker-files/npm/setup.sh
+++ docker-files/npm/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# npm cache clean --force
-# rm -rf node_modules package-lock.json
+npm cache clean --force
+rm -rf node_modules package-lock.json
 git config --global --add safe.directory /app
 npm install
 
