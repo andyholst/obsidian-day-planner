48bed7c84db1838b9de866e3bc0120e4fa571c71
Andy Holst <andy.holst@protonmail.com>
2024-12-13 23:41:36 +0100

    upload

--- docker-files/npm/setup.sh
+++ docker-files/npm/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-rm -rf node_modules package-lock.json
+#rm -rf node_modules package-lock.json
 git config --global --add safe.directory /app
-npm cache clean --force
+# npm cache clean --force
 export npm_config_platform=linux
 npm install --no-package-lock --loglevel=silly --prefer-offline --no-audit --ignore-scripts
 npm ci --no-package-lock --loglevel=silly --prefer-offline --no-audit --ignore-scripts
