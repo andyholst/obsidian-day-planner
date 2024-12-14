aea72e3efdedf967430c1db7ff1297c976623cec
Andy Holst <andy.holst@protonmail.com>
2024-12-14 01:48:02 +0100

    Updated setup.sh to keep npm cache

--- docker-files/npm/setup.sh
+++ docker-files/npm/setup.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-#rm -rf node_modules package-lock.json
 git config --global --add safe.directory /app
-# npm cache clean --force
 export npm_config_platform=linux
 npm ci --no-package-lock --loglevel=silly --prefer-offline --no-audit --ignore-scripts
 
